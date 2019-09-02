0fd1c6305b1934d11d0c7c16ea15a00d1dd05681
Armin Blaze <box_for_spam@mail.ru>
2019-09-02 14:12:40 +0300

    3 - Все обертки добавил в index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from 'components/App/App';
+import ErrorBoundry from 'components/ErrorBoundry/ErrorBoundry'
+import { LoginServiceProvider } from 'components/LoginServiceContext/LoginServiceContext';
+import LoginService from "services/LoginService";
+import store from 'store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Hello!</h1>
-  )
-}
+const loginService = new LoginService();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={ store }>
+    <ErrorBoundry>
+      <LoginServiceProvider  value={ loginService }>
+        <Router>
+          <App />
+        </Router>
+      </LoginServiceProvider>
+    </ErrorBoundry>
+  </Provider>
+, document.getElementById('root'));
