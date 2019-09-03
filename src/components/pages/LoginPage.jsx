3d6ec79bd1a7227a38fcc44d2db6acaba1de8205
Armin Blaze <box_for_spam@mail.ru>
2019-09-03 18:06:38 +0300

    7 - Начал писать connect для LoginPage

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 const LoginPage = ({ isLoggedIn, onLogin }) => {
   if(isLoggedIn) return <Redirect to='/'/>;
@@ -16,4 +17,27 @@ const LoginPage = ({ isLoggedIn, onLogin }) => {
   )
 };
 
-export {LoginPage};
+const mapStateToProps = (state) => {
+  const {isLoggedIn} = state;
+
+  return {
+    isLoggedIn
+  }
+};
+
+//тут нужно сделать кнопку "вход"
+//нужен сервис для получения
+//нужен экшен запроса входа
+//нужен ...
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps;
+  return bindActionCreators({
+    // fetchBooks: fetchBooks(dispatch, bookstoreService),
+    fetchBooks: fetchBooks(bookstoreService),
+    onAddToCart: bookAddedToCart,
+  }, dispatch)
+};
+
+const connectedLoginPage = connect(mapStateToProps)(LoginPage);
+
+export {connectedLoginPage as LoginPage};
